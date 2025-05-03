d32ae4fc9d61b708961af5b618899147e42b18af
miguel <carros_veloz@hotmail.com>
2025-05-02 18:26:15 -0600

    Estructura del dashboard creado

--- proyecto/servidores/views.py
+++ proyecto/servidores/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+'''
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
@@ -22,4 +24,11 @@ def user_login(request):
                 return HttpResponse('Credenciales incorrectas')
     else:
         form = LoginForm()
-    return render(request, 'servidores/login.xhtml', {'form': form})
+    return render(request, 'servidores/login.html', {'form': form})
+'''
+
+@login_required
+def dashboard(request):
+    return render(request,
+                  'servidores/dashboard.html',
+                  {'section': 'dashboard'})
